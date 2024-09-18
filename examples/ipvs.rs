333b824e868d309e53d3e541e769b64b0d305b84
David Ventura <davidventura27@gmail.com>
2024-09-18 17:34:10 +0200

    call broken NewService

--- examples/ipvs.rs
+++ examples/ipvs.rs
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
+use std::net::{IpAddr, Ipv4Addr};
 
 use netlink_packet_core::{
     NetlinkMessage, NetlinkPayload, NLM_F_DUMP, NLM_F_REQUEST,
 };
 use netlink_packet_generic::GenlMessage;
-use netlink_packet_ipvs::ctrl::{nlas::IpvsCtrlAttrs, IpvsCtrl, IpvsCtrlCmd};
+use netlink_packet_ipvs::ctrl::{nlas, IpvsCtrl, IpvsCtrlCmd};
 use netlink_sys::{protocols::NETLINK_GENERIC, Socket, SocketAddr};
 
 fn main() {
@@ -12,17 +13,36 @@ fn main() {
     socket.bind_auto().unwrap();
     socket.connect(&SocketAddr::new(0, 0)).unwrap();
 
+    let s = nlas::Service {
+        address: IpAddr::V4(Ipv4Addr::new(1, 2, 3, 4)),
+        netmask: nlas::Netmask(16, nlas::AddressFamily::IPv4), // 255.255.0.0 is a /16 netmask
+        scheduler: nlas::Scheduler::RoundRobin, // "rr" in the command
+        flags: nlas::Flags(0),                  // Assuming no flags are set
+        port: Some(9999),
+        fw_mark: None,             // Not specified in the command
+        persistence_timeout: None, // Not specified in the command
+        family: nlas::AddressFamily::IPv4,
+        protocol: nlas::Protocol::TCP, // '-t' in the command indicates TCP
+        stats: nlas::Stats,            // Assuming default Stats
+        stats64: nlas::Stats64,        // Assuming default Stats64
+    };
     let mut genlmsg = GenlMessage::from_payload(IpvsCtrl {
-        cmd: IpvsCtrlCmd::GetService,
-        nlas: vec![],
+        //cmd: IpvsCtrlCmd::GetService,
+        //nlas: vec![],
+        cmd: IpvsCtrlCmd::NewService,
+        nlas: s.create_nlas(),
     });
+    println!("{:?}", s.create_nlas());
     genlmsg.finalize();
     let mut nlmsg = NetlinkMessage::from(genlmsg);
-    nlmsg.header.flags = NLM_F_REQUEST | NLM_F_DUMP;
+    // TODO: DUMP for GET, remove DUMP for SET
+    //nlmsg.header.flags = NLM_F_REQUEST | NLM_F_DUMP;
+    nlmsg.header.flags = NLM_F_REQUEST;
     nlmsg.finalize();
 
     let mut txbuf = vec![0u8; nlmsg.buffer_len()];
     nlmsg.serialize(&mut txbuf);
+    println!("{:?}", txbuf);
 
     socket.send(&txbuf, 0).unwrap();
 
@@ -66,6 +86,6 @@ fn main() {
     }
 }
 
-fn print_entry(entry: Vec<IpvsCtrlAttrs>) {
+fn print_entry(entry: Vec<nlas::IpvsCtrlAttrs>) {
     println!("todo {entry:?}");
 }
